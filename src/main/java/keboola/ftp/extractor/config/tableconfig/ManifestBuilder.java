db7581e97f0471232ab5f618f64e2e97d34aa2e3
davidesner <esnerda@gmail.com>
2016-03-17 09:39:10 +0100

    manifest file json writer,default enclosure

--- src/main/java/keboola/ftp/extractor/config/tableconfig/ManifestBuilder.java
+++ src/main/java/keboola/ftp/extractor/config/tableconfig/ManifestBuilder.java
@@ -20,19 +20,18 @@ public class ManifestBuilder {
     public static void buildManifestFile(ManifestFile file, String folderPath, String resFileName) throws IOException {
         /*Build manifest file*/
         File resFile = new File(folderPath + File.separator + resFileName + ".manifest");
-//        ObjectMapper mapper = new ObjectMapper(new JsonFactory());
-//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        mapper.writeValue(resFile, file);
+        ObjectMapper mapper = new ObjectMapper(new JsonFactory());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.writeValue(resFile, file);
 
-        String manifest = "destination: " + resFileName + "\n"
-                + "incremental: " + file.isIncremental() + "\n"
-                + "delimiter: \"" + file.getDelimiter() + "\"\n"
-                + "enclosure: \"" + file.getEnclosure() + "\"\n";
-        if (file.getPrimaryKey() != null) {
-            manifest += "primary_key: \"" + file.getPrimaryKey() + "\"";
-        }
-
-        Files.write(manifest, resFile, Charset.forName("UTF-8"));
-
+//        String manifest = "destination: " + resFileName + "\n"
+//                + "incremental: " + file.isIncremental() + "\n"
+//                + "delimiter: \"" + file.getDelimiter() + "\"\n"
+//                + "enclosure: \"" + file.getEnclosure() + "\"\n";
+//        if (file.getPrimaryKey() != null) {
+//            manifest += "primary_key: \"" + file.getPrimaryKey() + "\"";
+//        }
+//
+//        Files.write(manifest, resFile, Charset.forName("UTF-8"));
     }
 }
