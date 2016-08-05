10ae8d47f624cdadcf70519c9e1fbf52b2021e3e
davidesner <esnerda@gmail.com>
2016-08-05 18:32:53 +0200

    UPDATE: add config parameter

--- src/main/java/keboola/ftp/extractor/ftpclient/Client.java
+++ src/main/java/keboola/ftp/extractor/ftpclient/Client.java
@@ -1,15 +1,12 @@
 package keboola.ftp.extractor.ftpclient;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.SocketException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
