fa1107b78ff714e837d0f50b20a5a5a3160b87ac
David Esner <david.esner@etnetera.cz>
2017-10-25 18:59:01 +0100

    UPDATE add retry strategy for FTP(S) clients

--- src/main/java/keboola/ftp/extractor/ftpclient/FTPClient.java
+++ src/main/java/keboola/ftp/extractor/ftpclient/FTPClient.java
@@ -9,6 +9,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
 import javax.net.ssl.SSLException;
 
@@ -18,6 +20,13 @@ import org.apache.commons.net.ftp.FTPFileFilter;
 import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTPSClient;
 import org.apache.commons.net.util.TrustManagerUtils;
+
+import com.github.rholder.retry.RetryException;
+import com.github.rholder.retry.Retryer;
+import com.github.rholder.retry.RetryerBuilder;
+import com.github.rholder.retry.StopStrategies;
+import com.github.rholder.retry.WaitStrategies;
+import com.google.common.base.Predicates;
 
 import keboola.ftp.extractor.ftpclient.FTPClientBuilder.Protocol;
 import keboola.ftp.extractor.ftpclient.filters.FtpFilters;
@@ -31,12 +40,14 @@ import keboola.ftp.extractor.ftpclient.filters.FtpFilters;
 public class FTPClient implements IFTPClient {
 
 	public static final int DEFAUTL_SSL_PORT = 990;
+	private static final int MAX_RETRIES = 5;
     private String userName;
     private String pass;
     private String url;
     private Integer port;
     private final TimeZone hostTz;
     private org.apache.commons.net.ftp.FTPClient ftpClient;
+
 
     /**
      *
@@ -74,6 +85,8 @@ public class FTPClient implements IFTPClient {
 		}
 		//set buffer size
         this.ftpClient.setBufferSize(1024 * 1024);
+        
+
     }
 
     private void setUpDefaultFtpClient(Integer port) {
@@ -116,29 +129,49 @@ public class FTPClient implements IFTPClient {
      * @throws IOException
      */
     public boolean connect() throws SocketException, IOException {
-    	if (ftpClient.isConnected()) {
-    		return true;
+    	Callable<Boolean> callable = new Callable<Boolean>() {
+    	    public Boolean call() throws Exception {
+    	    	if (ftpClient.isConnected()) {
+    	    		return true;
+    	    	}
+    	    	ftpClient.connect(url, port);
+    	    	//login to server
+    	    	if (!ftpClient.login(userName, pass)) {
+    	    		ftpClient.logout();
+    	    		return false;
+    	    	}
+    	    	int reply = ftpClient.getReplyCode();
+    	    	
+    	    	if (!FTPReply.isPositiveCompletion(reply)) {
+    	    		ftpClient.disconnect();
+    	    		return false;
+    	    	}
+    	    	if (ftpClient instanceof FTPSClient){
+    	    		setFtpSecurity();
+    	    	}
+    	    	//passive mode
+    	    	ftpClient.enterLocalPassiveMode();
+    	    	return true;
+    	    }
+    	};
+
+    	Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+    	        .retryIfResult(Predicates.<Boolean>isNull())
+    	        .retryIfExceptionOfType(IOException.class)
+    	        .retryIfRuntimeException()
+    	        .withWaitStrategy(WaitStrategies.exponentialWait())
+    	        .withStopStrategy(StopStrategies.stopAfterAttempt(MAX_RETRIES))
+    	        .build();
+    	try {
+    	    return retryer.call(callable);
+    	} catch (RetryException e) {
+    	    throw new IOException("Failed to connect to the client. After " +  MAX_RETRIES + " attempts.", e);
+    	} catch (ExecutionException e) {
+    		throw new IOException("Failed to connect to the client.", e);
     	}
-        ftpClient.connect(url, this.port);
-        //login to server
-        if (!ftpClient.login(this.userName, this.pass)) {
-            ftpClient.logout();
-            return false;
-        }
-        int reply = ftpClient.getReplyCode();
-
-        if (!FTPReply.isPositiveCompletion(reply)) {
-            ftpClient.disconnect();
-            return false;
-        }
-        if (ftpClient instanceof FTPSClient){
-        	setFtpSecurity();
-        }
-        //passive mode
-        ftpClient.enterLocalPassiveMode();
-        return true;
     }
-
+    
+    
     private void setFtpSecurity() throws SSLException, IOException {
     	((FTPSClient) ftpClient).execPROT("P");
     }
@@ -202,36 +235,24 @@ public class FTPClient implements IFTPClient {
      * @throws FtpException
      */
     private Map<String, Date> downloadAllFilesByFilter(String remoteFolder, String localFolderPath, FTPFileFilter filter) throws FtpException {
-        Map<String, Date> downloadedFiles = new HashMap();
+    	Retryer<Map<String, Date>> retryer = RetryerBuilder.<Map<String, Date>>newBuilder()
+    	        .retryIfResult(Predicates.<Map<String, Date>>isNull())
+    	        .retryIfExceptionOfType(IOException.class)
+    	        .retryIfRuntimeException()
+    	        .withWaitStrategy(WaitStrategies.exponentialWait())
+    	        .withStopStrategy(StopStrategies.stopAfterAttempt(MAX_RETRIES))
+    	        .build();
 
         try {
-            reconnectIfNeeded();
+            connect();
         } catch (IOException ex) {
             throw new FtpException("Error connecting to ftp server. " + ex.getMessage(), ex);
         }
         try {
-
-            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);  
-            //change back to root first
-            ftpClient.changeWorkingDirectory("/");
-            ftpClient.changeWorkingDirectory(remoteFolder);
-            int returnCode = ftpClient.getReplyCode();
-            if (returnCode == 550) {
-                throw new FtpException("Remote folder: '" + remoteFolder + "' does not exist or is not a folder!", null);
-            }
-
-            //get list of files to download
-            FTPFile[] files = ftpClient.listFiles(null, filter);
-
-            for (FTPFile file : files) {
-                downloadFile(ftpClient.printWorkingDirectory(), file.getName(), localFolderPath);
-                downloadedFiles.put(file.getName(), file.getTimestamp().getTime());
-            }
-
-        } catch (IOException ex) {
+        	return retryer.call(getDownloadAllFilesByFilter(remoteFolder, localFolderPath, filter));
+        } catch (Exception ex) {
             throw new FtpException("Error downloading files from folder: " + remoteFolder + ". " + ex.getMessage(), ex);
         }
-        return downloadedFiles;
     }
 
     /**
@@ -268,42 +289,90 @@ public class FTPClient implements IFTPClient {
      * @throws FtpException
      */
     public Map<String, Date> downloadFile(String remoteFolder, String fileName, String localFilePath) throws FtpException {
-        FileOutputStream fos = null;
-        Map<String, Date> downloadedFiles = new HashMap();
-        try {
-            reconnectIfNeeded();
-            if (remoteFolder != null) {
-                ftpClient.changeWorkingDirectory(remoteFolder);
-            }
-            int returnCode = ftpClient.getReplyCode();
-            if (returnCode == 550) {
-                throw new FtpException("Remote folder does not exist!", null);
-            }
-
-            fos = new FileOutputStream(localFilePath + File.separator + fileName);
-
-            if (ftpClient.changeWorkingDirectory(fileName)) {
-                throw new FtpException("Unable to download this file: " + fileName + ". It is a directory.", null);
-            }
-            FTPFile[] files = ftpClient.listFiles(fileName);
-            if (files == null || files.length == 0) {
-            	return downloadedFiles;
-            }
-            this.ftpClient.retrieveFile(fileName, fos);
-            downloadedFiles.put(fileName, files[0].getTimestamp().getTime());
-        } catch (IOException ex) {
+        
+    	Retryer<Map<String, Date>> retryer = RetryerBuilder.<Map<String, Date>>newBuilder()
+    	        .retryIfResult(Predicates.<Map<String, Date>>isNull())
+    	        .retryIfExceptionOfType(IOException.class)
+    	        .retryIfRuntimeException()
+    	        .withWaitStrategy(WaitStrategies.exponentialWait())
+    	        .withStopStrategy(StopStrategies.stopAfterAttempt(MAX_RETRIES))
+    	        .build();
+    	FileOutputStream fos = null;    	
+        try {        	
+        	fos = new FileOutputStream(localFilePath + File.separator + fileName);
+        	return retryer.call(getDownloadFileCallable(remoteFolder, fileName, localFilePath, fos));
+        } catch (Exception ex) {
             throw new FtpException("Error downloading file: " + fileName + ". " + ex.getMessage(), ex);
-
         } finally {
             if (fos != null) {
                 try {
                     fos.close();
                 } catch (IOException ex) {
-
                 }
             }
         }
-        return downloadedFiles;
+        
 
     }
+    
+    public Callable getDownloadFileCallable(final String remoteFolder, final String fileName, final String localFilePath, final FileOutputStream fos){
+    	Callable<Map<String, Date>> callable = new Callable<Map<String, Date>>() {
+    	    public Map<String, Date> call() throws Exception {
+    	        Map<String, Date> downloadedFiles = new HashMap();
+    	    	reconnectIfNeeded();
+    	        if (remoteFolder != null) {
+    	            ftpClient.changeWorkingDirectory(remoteFolder);
+    	        }
+    	        int returnCode = ftpClient.getReplyCode();
+    	        if (returnCode == 550) {
+    	            throw new FtpException("Remote folder does not exist!", null);
+    	        }
+
+    	       
+
+    	        if (ftpClient.changeWorkingDirectory(fileName)) {
+    	            throw new FtpException("Unable to download this file: " + fileName + ". It is a directory.", null);
+    	        }
+    	        FTPFile[] files = ftpClient.listFiles(fileName);
+    	        if (files == null || files.length == 0) {
+    	        	return downloadedFiles;
+    	        }
+    	        ftpClient.retrieveFile(fileName, fos);
+    	        downloadedFiles.put(fileName, files[0].getTimestamp().getTime());
+    	        return downloadedFiles;
+    	    }
+    	};
+    	return callable;
+    	
+    }
+   
+	public Callable<Map<String, Date>> getDownloadAllFilesByFilter(final String remoteFolder, final String localFolderPath, final FTPFileFilter filter) {
+			Callable<Map<String, Date>> callable = new Callable<Map<String, Date>>() {
+			public Map<String, Date> call() throws Exception {
+				Map<String, Date> downloadedFiles = new HashMap();
+
+				ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+				// change back to root first
+				ftpClient.changeWorkingDirectory("/");
+				ftpClient.changeWorkingDirectory(remoteFolder);
+				int returnCode = ftpClient.getReplyCode();
+				if (returnCode == 550) {
+					throw new FtpException("Remote folder: '" + remoteFolder + "' does not exist or is not a folder!",
+							null);
+				}
+
+				// get list of files to download
+				FTPFile[] files = ftpClient.listFiles(null, filter);
+
+				for (FTPFile file : files) {
+					downloadFile(ftpClient.printWorkingDirectory(), file.getName(), localFolderPath);
+					downloadedFiles.put(file.getName(), file.getTimestamp().getTime());
+				}
+				return downloadedFiles;
+			}
+
+		};
+		return callable;
+
+	}
 }
